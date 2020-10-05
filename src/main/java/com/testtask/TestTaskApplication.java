ed7843608b69d8d3eb613d92f87deee053fe39fa
Ilyin Ilya <32632835+random29@users.noreply.github.com>
2020-10-06 02:22:15 +0300

    Fixed redundant dependency.
    Fixed redundant dependency.

--- src/main/java/com/testtask/TestTaskApplication.java
+++ src/main/java/com/testtask/TestTaskApplication.java
@@ -1,11 +1,7 @@
 package com.testtask;
 
-import org.jline.utils.AttributedString;
-import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.shell.jline.PromptProvider;
 
 @SpringBootApplication
 
